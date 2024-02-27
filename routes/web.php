8a6c3ae12eef2ecda27cddc5a5bce0d9835ef2ef
sornkea <kea3rd@gmail.com>
2024-02-27 11:38:30 +0700

    add post to front end controller

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,7 @@ Route::get('/about', function () {
     return view('about');
 })->name('about');
 
-Route::get('/posts', function () {
-    $posts = [
-        ['title' => 'Post 1', 'content' => 'Content for post 1'],
-        ['title' => 'Post 2', 'content' => 'Content for post 2'],
-        ['title' => 'Post 3', 'content' => 'Content for post 3'],
-    ];
-
-    return view('posts', ['posts' => $posts]);
-});
+Route::get('/posts', [FrontendController::class, 'posts']);
 
 Route::get('/products/{id}', function ($id) {
     $product = [
