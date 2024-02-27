f9635d1ae7e5a10bbc90822ee50fd52a086f6c7d
sornkea <kea3rd@gmail.com>
2024-02-27 11:05:56 +0700

    add new style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,19 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
-<style>
-    body{
-        padding: 20px;
-    }
-    ul{
-        display: flex;
-        list-style: none;
-    }
-    ul li {
-        padding: 20px;
-    }
-</style>
 <body>
 <header>
     <h1>My Website</h1>
